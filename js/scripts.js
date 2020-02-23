687da0d4f4694c2d1fd4fb4822eef528691e3dea
Faith <kavesumulili@gmail.com>
2020-02-23 15:37:30 +0300

    Add submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+function show() {
+  var name =document.getElementById("name");
+  var email =document.getElementById("name");
+  var messsage =document.getElementById("name");
+}
 $("#dev").click(function(){
     $("dev").toggle(1000);
   });
