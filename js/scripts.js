9fd642c97e53431d4523ee2beea748a7d8fc7196
Faith <kavesumulili@gmail.com>
2020-02-24 12:54:37 +0300

    Add README.md

--- js/scripts.js
+++ js/scripts.js
@@ -4,27 +4,29 @@ $(document).ready(function() {
       var myEmail = $("#email").val();
       var comment = $("#message").val();
       if (myName === "" || myEmail === "" || comment === "") {
-          alert('Please fill in the required data');
-      } else {
-          alert(myName + ' ,we have received your message.Thank you for reaching out to us.');
-      }
-    })
-  })
+        alert('Please fill in the required data');
+    } else {
+      alert(myName + "we have received your message.Thank you for reaching out to us.");
+      event.preventDefault();
+        $("#name").val("");
+        $("#email").val("");
+        $("#message").val("");
+    }
+  });
 $(document).ready(function() {
   $(".design1").click(function(){
     $("#img1").toggle();
     $(".message1").toggle();
   });
 })
-$("#dev").ready(function() { 
-  $("#dev").toggle();
-    $(".img2").toggle();
+$(document).ready(function() { 
+  $(".dev").toggle();
+    $("#img2").toggle();
     $(".message2").toggle();
   });
 $(document).ready(function() {
   $("product").toggle();
     $(".img3").toggle();
         $("message3").toggle();
-  });
-  
-  
+})
+})
