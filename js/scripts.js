5f5df50d0adc7e0907f954642c05a0ff52ce157d
Faith <kavesumulili@gmail.com>
2020-02-24 08:27:55 +0300

    Add icons in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,32 @@
-function show() {
-  var name =document.getElementById("name");
-  var email =document.getElementById("name");
-  var messsage =document.getElementById("name");
-}
+$(document).ready(function() {
+  $('.contact').submit(function(event) {
+      var myName = $('#name').val();
+      var myEmail = $('#email').val();
+      var comment = $('#message').val();
+      if (myName === '' || myEmail === '' || comment === '') {
+          alert('Please fill in the required data');
+      } else {
+          alert(myName + ' ,we have received your message.Thank you for reaching out to us.');
+      }
+    })
+  })
 $("#dev").click(function(){
-    $("dev").toggle(1000);
+    $(".message1").toggle(1000);
   });
   
   $("#design").click(function(){
-    $("design").toggle(1000);
+    $(".message2").toggle(1000);
   });
+  $(document).ready(function() {
+    $('#dev').click(function() {
+        $('images/design_icon.png').toggle();
+    })
+  })
+  $("#dev").click(function(){
+    $(".message1").hide();
+  });
+  
+  $("#dev").click(function(){
+    $(".message2").show();
+  });
+  
