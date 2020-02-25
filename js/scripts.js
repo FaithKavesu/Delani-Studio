f47e7d1e8346940fedc55d01f75c5a89e0bf9716
Faith <kavesumulili@gmail.com>
2020-02-25 08:22:38 +0300

    Add changes to message display

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,36 @@
-$(document).ready(function() {
-  $
-}
+function response() {
+      var myName = $("input#name").val();
+      var myEmail = $("input#email").val();
+      var comment = $("input#message").val();
+      alert(myName + ",we have received your message.Thank you for reaching out to us.");
+      event.preventDefault();
+      $('#name').val('');
+      $('#email').val('');
+      $('#textarea').val('');
+    }
+    $(document).ready(function() {  
+  $("#img1").click(function(){
+    $("#img1").slideToggle();
+    $("#message1").toggle();
+  });
+  $("#message1").click(function(){
+    $("#img1").slideToggle();
+    $("#message1").toggle();
+  });
+  $(".dev").click(function() {
+    $("#img2").slideToggle();
+    $(".message2").toggle();
+  });
+  $("#img2").click(function() {
+    $("#img2").slideToggle();
+    $(".message2").toggle();
+  })
+  $("#message3").click(function() {
+    $(".img3").slideToggle();
+        $("#message3").slideToggle();
+})
+  $(".img3").click(function() {
+    $(".img3").slideToggle();
+      $("#message3").slideToggle();
+})
+    })
